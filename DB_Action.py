72316537048429728b6683c548ffead80f3d6990
yahelfri <49039385+yahelfri@users.noreply.github.com>
2020-04-04 23:23:46 +0300

    Update DB_Action.py

--- DB_Action.py
+++ DB_Action.py
@@ -1,14 +1,17 @@
+
 from django.contrib import admin
 from django.urls import path
 import pymongo
 from pymongo import MongoClient
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
 collection: MongoClient.address
-
-cluster = MongoClient("mongodb+srv://yahelfr:29913051@cluster0-ufupc.mongodb.net/test?retryWrites=true&w=majority")  ## connect to cluster
-db = cluster["tryout"]  ##connect to database
-collection = db["students"]  ##connect to collection
-
-star = {"_id": 50 , "name": "elhanan", "score": 15}
+cluster = MongoClient("mongodb+srv://yahelfr:29913051@cluster0-4iuqq.mongodb.net/test?retryWrites=true&w=majority")  ## connect to cluster
+db = cluster["shareball"]  ##connect to database
+collection = db["users"]  ##connect to collection
+star = {"_id": 20 , "name": "yahel", "score": 10}
 collection.insert_one(star)
 
