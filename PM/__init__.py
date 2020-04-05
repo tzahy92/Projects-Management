caa0104a31fc18f32e31c72f1793592751c59811
yahelfri <49039385+yahelfri@users.noreply.github.com>
2020-04-05 23:02:18 +0300

    log in page, first version3

--- PM/__init__.py
+++ PM/__init__.py
@@ -1,8 +1,10 @@
 from . import DB_Action
 
-x = DB_Action.get_user_by_ID("20")
-DB_Action.update_user_by_ID(20)
-z = ""
+##x = DB_Action.get_user_by_ID("20")
+newUser = {"_id":"23","userName":"king","password":"2323","firstName":"lebron","lastName":"james","role":1}
+##DB_Action.insert_user("24","user","user","user","user",1)
+##DB_Action.update_user_by_ID(20)
+
 
 
 
