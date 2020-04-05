e520bcdfb5e057c97ac6e43af819fc49ec0206a0
Elhanan <bsnani7@gmail.com>
2020-04-05 20:21:30 +0300

    changes: setting

--- PM/__init__.py
+++ PM/__init__.py
@@ -1,8 +1,7 @@
-from . import DB_Action
+from PM import DB_Action
 
 x = DB_Action.get_user_by_ID("20")
-DB_Action.update_user_by_ID(20)
-z = ""
+DB_Action.update_user_by_ID("20")
 
 
 
--- PM/settings.py
+++ PM/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PM',
-
 ]
 
 MIDDLEWARE = [
@@ -127,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/admin'
