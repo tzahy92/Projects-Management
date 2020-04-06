9121ba3e542ea15ad0708a11412669245baed002
yahelfri <49039385+yahelfri@users.noreply.github.com>
2020-04-06 10:19:24 +0300

    log in page, first version5

--- PM/DB_Action.py
+++ PM/DB_Action.py
@@ -12,14 +12,23 @@ usersCollection = db["users"]  ##connect to collection
 
 
 def insert_user(_id,userName,password,firstName,lastName,role):
-    newUser = {"_id": _id,"userName" : userName,"password" : password, "firstName": firstName,"lastName":lastName, "role": role}
-    usersCollection.insert_one((newUser))
+    if(get_user_by_userName(userName)!=None):
+        raise Exception("this username is taken")
+    if(get_user_by_ID(_id)!=None):
+        raise Exception("this ID is taken")
+    else:
+        newUser = {"_id": _id,"userName" : userName,"password" : password, "firstName": firstName,"lastName":lastName, "role": role}
+        usersCollection.insert_one((newUser))
 
 def get_user_by_ID(_ID):
     return usersCollection.find_one({"_id":20,"name":"yahel"})
 
-def update_user_by_ID(_ID):
-    usersCollection.update_one({"_id":20},{"$set":{"score":"30"}})
+def update_user_by_ID(_ID,field,value):
+    if (field == "userName" and get_user_by_userName(value) != None):
+        raise Exception("this username is taken")
+    if (field == "id" and get_user_by_ID(field) != None):
+        raise Exception("this ID is taken")
+    usersCollection.update_one({"_id":20},{"$set":{field:value}})
 
 def get_user_by_userName(username):
     return usersCollection.find_one({"userName": username})
