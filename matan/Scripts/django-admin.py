3ad50e368f4c247622e5ceebd3062d75197148a7
Matan Lazimi <matanlazimi@gmail.com>
2020-04-06 14:51:18 +0300

    manager page

---
+++ matan/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\matan\PycharmProjects\Projects-Management\matan\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
