80e0a67413643e14c0e9aa0e1cf6d2c4de1f2b11
יצחק מימון <tzahy92@gmail.com>
2020-04-06 14:33:24 +0300

    taest 0.1

--- trainer/tests.py
+++ trainer/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 # Create your tests here.
-from PM import DB_Action
+import PM.DB_Action
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_insert_to_DB(self):
         with self.assertRaisesRegex(Exception,"this ID is taken"):
-            DB_Action.insert_user("23", "tzahy", "1234", "lab", "ja", 1)
+            PM.DB_Action.insert_user("23", "tzahy", "1234", "lab", "ja", 1)
 
 if __name__ == '__main__':
     unittest.main()
