750e0316786cd6b198aa0b4558c6e08918ca395b
Matan Lazimi <matanlazimi@gmail.com>
2020-04-06 14:45:17 +0300

    manager page

--- PM/__init__.py
+++ PM/__init__.py
@@ -14,7 +14,7 @@ import random
 #firstnames = ["yossi","shimi","avi","aviel","motti","gall","static","ben-el","eden","lea","ruti","simcha","avihu","ivri"]
 #lastnames = ["cohen","levy","friedman","tavori","shilo","bandel","maymon"]
 #for i in range(50):
-#       DB_Action.insert_user("10"+str(i),"admin"+str(i), "1234", random.choice(firstnames),random.choice(lastnames),1)
+#       DB_Action.insert_user("10"+str(i),"admini"+str(i), "1234", random.choice(firstnames),random.choice(lastnames),1)
 #      DB_Action.insert_user("40"+str(i),"user"+str(i),"1234",random.choice(firstnames),random.choice(lastnames),3)
 #     DB_Action.insert_user("30"+str(i),"trainer"+str(i),"1234",random.choice(firstnames), random.choice(lastnames),2)
 
